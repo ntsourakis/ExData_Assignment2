8366cc261623cb63d03482c48967412bfd2e99c4
tsouraki <Nikolaos.Tsourakis@unige.ch>
2014-07-27 23:30:04 +0200

    Assignment 2

--- plot2.R
+++ plot2.R
@@ -7,16 +7,16 @@ library(ggplot2)
 NEI <- readRDS("summarySCC_PM25.rds")
 
 # Read the data of interest
-data <- with(NEI[which(NEI$fips == "24510"), aggregate(Emissions, by = list(year), sum))
-colnames(data) <- c("year", "Emissions")
+data <- with(NEI[which(NEI$fips == "24510"), ], aggregate(Emissions, by = list(year), sum))
+colnames(aggregate.24510) <- c("year", "Emissions")
 
 # Use white background
 par(bg = "white")
 
 # Make the plot
 plot(data, type = "o", ylab = expression("Emissions"), 
-    xlab = "Year", main = "Total Emissions in Baltimore")
-
+    xlab = "Year", main = "Emissions for Baltimore County", xlim = c(1999, 2008))
+        
 # Copy the plot to a PNG file
 dev.copy(png,file="plot2.png")
 
